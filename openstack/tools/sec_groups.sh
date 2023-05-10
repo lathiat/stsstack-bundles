279ae4f59cbad025ef9b4d1294b5692f0d005007
Trent Lloyd <trent.lloyd@canonical.com>
2023-05-10 13:20:06 +0800

    sec_groups: Use a single allow any protocol rule
    Drop the individual port access and allow access on any protocol/port
    instead.

--- openstack/tools/sec_groups.sh
+++ openstack/tools/sec_groups.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -ex
 # Add sec groups for basic access
 `env| egrep -q "^OS_.*DOMAIN.*|/v3"` && v3args="--project-domain admin_domain" || v3args=""
-secgroup=${1:-`openstack security group list --project admin $v3args| grep default| awk '{print $2}'`}
-for port in 22 53 80 443; do
-    openstack security group rule create $secgroup --protocol tcp --remote-ip 0.0.0.0/0 --dst-port $port --project admin $v3args ||:
-done
+secgroup=${1:-`openstack security group show default -f value -c id`}
 
-openstack security group rule create $secgroup --protocol icmp --remote-ip 0.0.0.0/0 --project admin $v3args ||:
+openstack security group rule create $secgroup --protocol any --ethertype IPv4
+openstack security group rule create $secgroup --protocol any --ethertype IPv6
